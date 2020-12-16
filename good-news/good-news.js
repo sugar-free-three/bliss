8c39eb3cf75d0254a782301109a6fa92b0fbeb57
shanniehoang <50119078+shanniehoang@users.noreply.github.com>
2020-12-15 23:52:16 -0800

    implemented comments

--- good-news/good-news.js
+++ good-news/good-news.js
@@ -1,1 +1,13 @@
-// if i have time, i will add functions for the comments
+// adds a new comment where text is the comment and cNum is which numbered comment it is
+function addComment(cNum){
+    var text = document.getElementsByClassName("user-request-text")[cNum].value;
+    var userText= '<div class="media"><img class="mr-1" src="https://www.flaticon.com/svg/static/icons/svg/763/763713.svg" alt="icon" height="50" width="50"><div class="media-body col-sm-auto"><h6 class="mt-0">You</h6><p>'
+    userText+= text;
+    userText+='</p></div>';
+
+    var comment = $.parseHTML(userText);
+    var section = document.getElementsByClassName("user-comment")[cNum];
+    $(comment).appendTo(section);
+
+    // possible todo: clear the input box to be clean
+}
