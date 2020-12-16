3eca6bd44f986c4e1879e3b0a14d6d04c35807b3
shanniehoang <50119078+shanniehoang@users.noreply.github.com>
2020-12-16 11:47:52 -0800

    more comment sections edits

--- good-news/good-news.js
+++ good-news/good-news.js
@@ -1,9 +1,9 @@
 // adds a new comment where text is the comment and cNum is which numbered comment it is
 function addComment(cNum){
     var text = document.getElementsByClassName("user-request-text")[cNum].value;
-    var userText= '<div class="media"><img class="mr-1" src="https://www.flaticon.com/svg/static/icons/svg/763/763713.svg" alt="icon" height="50" width="50"><div class="media-body col-sm-auto"><h6 class="mt-0">You</h6><p>'
+    var userText= '<li><div class="media"><img class="mr-1" src="https://www.flaticon.com/svg/static/icons/svg/763/763713.svg" alt="icon" height="50" width="50"><div class="media-body col-sm-auto"><h6 class="mt-0">You</h6><p>'
     userText+= text;
-    userText+='</p></div>';
+    userText+='</p></div></li><hr/>';
 
     var comment = $.parseHTML(userText);
     var section = document.getElementsByClassName("user-comment")[cNum];
