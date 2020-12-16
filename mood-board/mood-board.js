870e7a2a0c099cb3f89448a4a5caeadc2a99778b
Joyce Luu <luuj11@uci.edu>
2020-12-16 01:58:43 -0800

    adding animations, note creation bar, note colours!

--- mood-board/mood-board.js
+++ mood-board/mood-board.js
@@ -1,11 +1,23 @@
 let count = 0;
 let cap = 11;
 
-function addNote() {
+function addNote(button) {
   if (count == cap) {
     return;
   }
-  var n = $.parseHTML("<div class='stickynote'><textarea rows='4' cols='27'></textarea></div>");
+  let colour = button.id;
+
+  if (colour == "pink") {
+    colour = "#f0cdcd";
+  }
+  else if (colour == "blue") {
+    colour = "#c9dee6";
+  }
+  else {
+    colour = "#f3d9c4";
+  }
+
+  var n = $.parseHTML("<div class='stickynote'" + " style='background-color:" + colour + ";'><textarea rows='4' cols='27'></textarea></div>");
   
   var board = 
     document.getElementsByClassName("board")[0];
@@ -13,6 +25,24 @@ function addNote() {
   count++;
   $(n).appendTo(board);
 }
+
+function saveNote() {
+  localStorage.setItem(count, {})
+}
+
+$(document).ready(function() {
+  var board = document.getElementsByClassName("board")[0];
+  console.log(localStorage.getItem(0));
+  if (localStorage.getItem(0) != null) {
+    let c = 0;
+    var note;
+    while (localStorage.getItem(c) != null) {
+      note = localStorage.getItem(c);
+      console.log(note);
+      c++;
+    }
+  }
+});
 
 
 
